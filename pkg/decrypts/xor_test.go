03916521e2336bcc5b54e8560c3c05ef4eb9df85
Tung Le Vo <letung3105@gmail.com>
2019-08-07 15:19:09 +0700

    improve decrypt single byte xor tests

--- pkg/decrypts/xor_test.go
+++ pkg/decrypts/xor_test.go
@@ -9,12 +9,21 @@ import (
 
 func TestSingleByteXOR(t *testing.T) {
 	t.Parallel()
-	plain := []byte("This is a testing plain text for single byte xor encryption")
-	key := byte('K')
+	tt := []struct {
+		plain []byte
+		key   byte
+	}{
+		{plain: []byte("This is a testing plain text for single byte xor encryption"), key: byte('K')},
+		{plain: []byte("short sentence!"), key: byte('K')},
+		{plain: []byte("This! Contain punctuations?!"), key: byte('K')},
+		{plain: []byte(""), key: byte('K')},
+	}
 
-	cipher := crypts.SingleByteXOR(plain, key)
-	output := SingleByteXOR(cipher)
-	if !reflect.DeepEqual(output, plain) {
-		t.Errorf("Incorrect plain text: got %s, expected %s", output, plain)
+	for _, tc := range tt {
+		cipher := crypts.SingleByteXOR(tc.plain, tc.key)
+		output := SingleByteXOR(cipher)
+		if !reflect.DeepEqual(output, tc.plain) {
+			t.Errorf("Incorrect plain text: got %s, expected %s", output, tc.plain)
+		}
 	}
 }
