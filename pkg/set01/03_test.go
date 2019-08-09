4de6512dd78f563eb309797f6ee70bf01b977d60
Tung Le Vo <letung3105@gmail.com>
2019-08-09 14:12:45 +0700

    hassle

--- pkg/set01/03_test.go
+++ pkg/set01/03_test.go
@@ -6,27 +6,31 @@ import (
 	"testing"
 
 	"github.com/letung3105/cryptogophers/pkg/crypts"
+	"github.com/pkg/errors"
 )
 
 func TestHexSingleXORDecrypt(t *testing.T) {
 	t.Parallel()
 	test := struct {
-		in []byte
+		inHex []byte
 	}{
 		[]byte("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"),
 	}
 
-	out, key, _, err := HexSingleXORDecrypt(test.in)
+	out, key, _, err := HexSingleXORDecrypt(test.inHex)
 	if err != nil {
 		t.Fatalf("unexpected error: %+v", err)
 	}
 
-	constructed := crypts.SingleXOR(out, key)
-	constructedHex := make([]byte, hex.EncodedLen(len(constructed)))
-	n := hex.Encode(constructedHex, constructed)
-	constructedHex = constructedHex[:n]
+	in := make([]byte, hex.EncodedLen(len(test.inHex)))
+	n, err := hex.Decode(in, test.inHex)
+	in = in[:n]
+	if err != nil {
+		t.Fatal(errors.Wrapf(err, "could not decode: %s", test.inHex))
+	}
 
-	if !bytes.Equal(constructedHex, test.in) {
-		t.Errorf("incorrect reconstructed input:\nhave %s\nwant %s", constructedHex, test.in)
+	constructed := crypts.SingleXOR(out, key)
+	if !bytes.Equal(constructed, in) {
+		t.Errorf("incorrect reconstructed input:\nhave %x\nwant %x", constructed, in)
 	}
 }
